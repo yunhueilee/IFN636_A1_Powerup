ef829230ee2a3c954b3a2762265002c825c74bd6
fay.yunhuei <fay.yunhuei@gmail.com>
2026-08-26 15:23:17 +1000

    FCBS-24 Epic 6: Testing and EC2 Deployment

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,1 +1,3 @@
-export const API_URL = 'http://10.88.59.254:5001/api';
+// export const API_URL = 'http://10.88.59.254:5001/api';
+export const API_URL =
+  process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5001/api';
